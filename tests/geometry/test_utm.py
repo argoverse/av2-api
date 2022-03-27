afd0c124f9df72c54a36abe09205ae0294f8936e
John Lambert <jlambert@argo.ai>
2022-03-26 20:25:39 -0400

    add NDArrayFloat annotations on test cases

--- tests/geometry/test_utm.py
+++ tests/geometry/test_utm.py
@@ -7,12 +7,12 @@ import numpy as np
 
 import av2.geometry.utm as geo_utils
 from av2.geometry.utm import CityName, CITY_ORIGIN_LATLONG_DICT
+from av2.utils.typing import NDArrayFloat
 
 
 def test_convert_city_coords_to_wgs84_atx() -> None:
     """Convert city coordinates from Austin, TX to GPS coordinates."""
-
-    points_city = np.array(
+    points_city: NDArrayFloat = np.array(
         [
             [1745.37, -1421.37],
             [1738.54, -1415.03],
@@ -22,7 +22,7 @@ def test_convert_city_coords_to_wgs84_atx() -> None:
 
     wgs84_coords = geo_utils.convert_city_coords_to_wgs84(points_city, city_name=CityName.ATX)
 
-    expected_wgs84_coords = np.array(
+    expected_wgs84_coords: NDArrayFloat = np.array(
         [
             [30.261642967615092, -97.72246957081633],
             [30.26170086362131, -97.72253982250783],
@@ -34,8 +34,7 @@ def test_convert_city_coords_to_wgs84_atx() -> None:
 
 def test_convert_city_coords_to_wgs84_wdc() -> None:
     """Convert city coordinates from Washington, DC to GPS coordinates."""
-
-    points_city = np.array(
+    points_city: NDArrayFloat = np.array(
         [
             [1716.85, 4470.38],
             [2139.70, 4606.14],
@@ -43,7 +42,7 @@ def test_convert_city_coords_to_wgs84_wdc() -> None:
     )
 
     wgs84_coords = geo_utils.convert_city_coords_to_wgs84(points_city, city_name=CityName.WDC)
-    expected_wgs84_coords = np.array(
+    expected_wgs84_coords: NDArrayFloat = np.array(
         [
             [38.9299801515994, -77.0168603173312],
             [38.931286945069985, -77.0120195048271],
