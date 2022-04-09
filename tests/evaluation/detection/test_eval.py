0b00475e7b47eca36c4931200a834231b4142830
Benjamin Wilson <benjaminrwilson@noreply.github.com>
2022-04-09 14:45:32 +0000

    Remove unused import.

--- tests/evaluation/detection/test_eval.py
+++ tests/evaluation/detection/test_eval.py
@@ -32,7 +32,7 @@ from av2.geometry.iou import iou_3d_axis_aligned
 from av2.map.map_api import ArgoverseStaticMap
 from av2.structures.cuboid import ORDERED_CUBOID_COL_NAMES
 from av2.utils.constants import PI
-from av2.utils.io import read_city_SE3_ego, read_feather, write_img
+from av2.utils.io import read_city_SE3_ego, read_feather
 from av2.utils.typing import NDArrayBool, NDArrayFloat
 
 TEST_DATA_DIR: Final[Path] = Path(__file__).parent.resolve() / "data"
