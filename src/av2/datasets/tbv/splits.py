4743e81719a2cae4a5680ee71a97bedcaa82bc98
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2022-04-12 17:44:00 -0400

    Update splits.py

--- src/av2/datasets/tbv/splits.py
+++ src/av2/datasets/tbv/splits.py
@@ -1,16 +1,3 @@
 # <Copyright 2022, Argo AI, LLC. Released under the MIT license.>
 
 """Splits for the Trust, but Verify (Argoverse 2 Map Change) dataset."""
-<<<<<<< HEAD
-
-from typing import Final, Tuple
-
-TRAIN: Final[Tuple[str, ...]] = ()
-
-
-VAL: Final[Tuple[str, ...]] = ()
-
-
-TEST: Final[Tuple[str, ...]] = ()
-=======
->>>>>>> 3fd9059fca8ca8386de0abe56b429c8bd2bbde9d
