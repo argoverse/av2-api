37f0517d1a8a141a5a8b7f11a9e161a96b7c2bb7
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2022-04-29 16:01:08 -0400

    Add rendering scripts.

--- src/av2/rendering/graphics.py
+++ src/av2/rendering/graphics.py
@@ -4,10 +4,14 @@ from typing import Final, List
 
 import numpy as np
 import vedo
+from av2.geometry.se3 import SE3
+from av2.map.map_api import ArgoverseStaticMap
 
 from av2.structures.cuboid import CuboidList
 from av2.structures.sweep import Sweep
+from av2.structures.timestamped_image import TimestampedImage
 from av2.utils.typing import NDArrayByte, NDArrayFloat
+import cv2
 
 MESH_EDGES: Final[List[List[int]]] = [
     [0, 1, 2],
@@ -23,7 +27,7 @@ MESH_EDGES: Final[List[List[int]]] = [
 ]
 
 COLORS: NDArrayByte = np.array(
-    [[255, 255, 255], [255, 255, 255]] + [[0, 0, 255] for _ in MESH_EDGES[2:]],
+    [[128, 128, 128], [128, 128, 128]] + [[0, 0, 255] for _ in MESH_EDGES[2:]],
     dtype=np.uint8,
 )
 
@@ -31,18 +35,28 @@ COLORS: NDArrayByte = np.array(
 def plotter():
     return vedo.Plotter(
         title="AV2",
+        # shape="1|7",
         interactive=False,
         offscreen=False,
-        bg="black",
+        bg=[32,32,32],
         sharecam=False,
         resetcam=False,
         # backend="k3d",
     )
 
+def imagery(imagery: List[TimestampedImage]):
+    # breakpoint()
+    imgs = [cv2.cvtColor(v.img, cv2.COLOR_BGR2RGB) for v in imagery.values()]
+    imgs = [vedo.Picture(v) for v in imgs]
+    return [vedo.Picture(v) for v in imgs]
 
-def points(sweep: Sweep):
-    pts = vedo.Points(inputobj=sweep.xyz[..., :3].tolist(), r=4, c="grey")
-    pts = pts.cmap("Spectral", sweep.xyz[..., -1], vmin=-3, vmax=3)
+def egovehicle():
+    return vedo.Sphere(r=0.5, c="white")
+
+
+def points(sweep: Sweep, colors: List[List[int]]):
+    pts = vedo.Points(inputobj=sweep.xyz[..., :3].tolist(), r=4, c=colors).lighting("off")
+    # pts = pts.cmap("magma", sweep.xyz[..., -1], vmin=-5, vmax=5)
     return pts
 
 
@@ -51,7 +65,7 @@ def cuboids(annotations: CuboidList) -> List[vedo.Mesh]:
     meshes: List[vedo.Mesh] = [
         vedo.Mesh(
             [x, MESH_EDGES],
-            alpha=0.5,
+            alpha=0.4,
         ).lighting("off")
         for x in vertices
     ]
@@ -59,6 +73,23 @@ def cuboids(annotations: CuboidList) -> List[vedo.Mesh]:
         mesh.celldata["mycolors"] = COLORS
         mesh.celldata.select("mycolors")
     return meshes
+
+
+def lanes(avm: ArgoverseStaticMap, city_SE3_ego: SE3, alpha: float = 0.5):
+    lanes = []
+    lane_segments = avm.get_nearby_lane_segments(city_SE3_ego.translation[:2], search_radius_m=75.0)
+    for lane in lane_segments:
+        # avm.get_nearby_lane_segments()
+        left_lane = np.array([[point.x, point.y, point.z] for point in lane.left_lane_boundary.waypoints])
+        right_lane = np.array([[point.x, point.y, point.z] for point in lane.right_lane_boundary.waypoints])
+
+        left_lane = city_SE3_ego.inverse().transform_from(left_lane)
+        right_lane = city_SE3_ego.inverse().transform_from(right_lane)
+        left_lane = vedo.Line(left_lane.tolist(), lw=10, c="grey", alpha=alpha)
+        right_lane = vedo.Line(right_lane.tolist(), lw=10, c="grey", alpha=alpha)
+        lanes.append(left_lane)
+        lanes.append(right_lane)
+    return lanes
 
 
 def map():
--- tutorials/generate_rendered_scene.py
+++ tutorials/generate_rendered_scene.py
@@ -1,23 +1,62 @@
+# <Copyright 2022, Argo AI, LLC. Released under the MIT license.>
+
 from pathlib import Path
 
+import numpy as np
 import vedo
 
 from av2.datasets.sensor.sensor_dataloader import SensorDataloader
-from av2.rendering.graphics import cuboids, plotter, points
+from av2.rendering.graphics import cuboids, egovehicle, lanes, plotter, points
+from av2.utils.typing import NDArrayByte
 
 vedo.settings.allowInteraction = True
 
 
 def render_scene() -> None:
+    """Render a scene from the sensor dataset."""
     dataset_dir = Path.home() / "data" / "datasets" / "av2" / "sensor"
     dataloader = SensorDataloader(dataset_dir=dataset_dir)
 
     plot = plotter()
-    for datum in dataloader:
-        plot += points(datum.sweep)
+    video = vedo.Video(
+        fps=10,  # Lidar Hz
+    )
+    video.options = "-c:v libx264 -crf 24 -preset veryfast -pix_fmt yuv420p"
+    for i, datum in enumerate(dataloader):
+        plot += egovehicle()
         plot += cuboids(datum.annotations)
-        plot.show(camera={"pos": [-60, 0, 40], "viewup": [1, 0, 0]})
+        plot += lanes(datum.avm, datum.timestamp_city_SE3_ego_dict[datum.timestamp_ns])
+
+        pts = datum.sweep.xyz
+        city_SE3_ego = datum.timestamp_city_SE3_ego_dict[datum.timestamp_ns]
+        points_city = city_SE3_ego.transform_point_cloud(pts)
+        points_city = datum.avm.remove_ground_surface(points_city)
+        datum.sweep.xyz = city_SE3_ego.inverse().transform_point_cloud(points_city)
+
+        rgba: NDArrayByte = np.full((datum.sweep.xyz.shape[0], 4), dtype=np.uint8, fill_value=32)
+        rgba[..., -1] = 255
+        for _, v in datum.synchronized_imagery.items():
+            uv, _, is_valid = v.camera_model.project_ego_to_img_motion_compensated(
+                datum.sweep.xyz,
+                datum.timestamp_city_SE3_ego_dict[v.timestamp_ns],
+                datum.timestamp_city_SE3_ego_dict[datum.timestamp_ns],
+            )
+            uv_int = np.round(uv).astype(int)[is_valid]
+
+            rgb_cam = v.img[uv_int[..., 1], uv_int[..., 0]][..., ::-1]
+            rgba_cam = np.concatenate((rgb_cam, np.full_like(rgb_cam[..., -1:], fill_value=255)), axis=-1)
+            rgba[is_valid] = rgba_cam
+        plot += points(datum.sweep, colors=rgba.tolist())
+
+        plot.show(camera={"pos": [-60, 0, 20], "viewup": [1, 0, 0]}, interactive=False)
+        video.addFrame()
         plot.clear()
+
+        if i == 150:
+            video.name = f"{datum.log_id}.mp4"
+            video.close()
+            plot.close()
+            break
 
 
 if __name__ == "__main__":
