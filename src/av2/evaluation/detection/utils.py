dc68a702f7f9f562dbca624e4ef5e48d40c49e68
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2022-09-02 19:13:45 -0700

    Fix lint.

--- src/av2/evaluation/detection/utils.py
+++ src/av2/evaluation/detection/utils.py
@@ -452,7 +452,6 @@ def load_mapped_avm_and_egoposes(
     Raises:
         RuntimeError: If the process for loading maps and timestamped egoposes fails.
     """
-
     log_id_to_timestamped_poses = {log_id: read_city_SE3_ego(dataset_dir / log_id) for log_id in log_ids}
     avms: Optional[List[ArgoverseStaticMap]] = Parallel(n_jobs=-1, backend="threading")(
         delayed(ArgoverseStaticMap.from_map_dir)(dataset_dir / log_id / "map", build_raster=True) for log_id in log_ids
