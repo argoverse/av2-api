919fd8acb0ff06e816fdd1f41955cd4cb59e296c
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2023-03-13 15:34:34 -0700

    Fix lint.

--- tutorials/torch_dataloader.py
+++ tutorials/torch_dataloader.py
@@ -4,21 +4,20 @@ from pathlib import Path
 
 from tqdm import tqdm
 
-from av2.torch.dataloaders.scene_flow import SceneFlowDataloader
+from av2.torch.dataloaders.sensor import Dataloader
 
 
 def main() -> None:
     """Iterate over the dataloader."""
-
     # Dataset should live at ~/data/datasets/{dataset_name}/{split_name}
     root_dir = Path.home() / "data" / "datasets"
     dataset_name = "av2"
     split_name = "val"
     num_accum_sweeps = 1
 
-    dataloader = SceneFlowDataloader(root_dir, dataset_name, split_name, num_accum_sweeps=num_accum_sweeps)
-    for sweep, next_sweep in tqdm(dataloader):
-        print(sweep, next_sweep)
+    dataloader = Dataloader(root_dir, dataset_name, split_name, num_accum_sweeps=num_accum_sweeps)
+    for sweep in tqdm(dataloader):
+        print(sweep.lidar.as_tensor())
 
 
 if __name__ == "__main__":
