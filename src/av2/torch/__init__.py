5c560bdf7f8600d3040148c4e71f4485b7bc29de
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2023-03-14 16:02:07 -0700

    Update torch init.

--- src/av2/torch/__init__.py
+++ src/av2/torch/__init__.py
@@ -1,10 +1,1 @@
-"""AV2 Pytorch interface."""
-
-import logging
-
-logger = logging.getLogger(__name__)
-
-try:
-    import torch
-except ImportError as _:
-    logger.error("Please install Pytorch to use this module.")
+"""AV2 Pytorch api."""
