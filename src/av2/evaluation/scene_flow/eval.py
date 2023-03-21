f79c849a2950114b46360281f42a79b7e9ad4fec
nchodosh <37190375+nchodosh@users.noreply.github.com>
2023-03-21 10:54:16 -0400

    Update src/av2/evaluation/scene_flow/eval.py
    Co-authored-by: Benjamin Wilson <benjaminrwilson@users.noreply.github.com>

--- src/av2/evaluation/scene_flow/eval.py
+++ src/av2/evaluation/scene_flow/eval.py
@@ -31,7 +31,7 @@ def compute_end_point_error(dts: NDArrayFloat, gts: NDArrayFloat) -> NDArrayFloa
     return end_point_error
 
 
-def compute_accuracy(dts: NDArrayFloat, gts: NDArrayFloat, threshold: float) -> NDArrayFloat:
+def compute_accuracy(dts: NDArrayFloat, gts: NDArrayFloat, distance_threshold: float) -> NDArrayFloat:
     """Compute the percent of inliers for a given threshold for a set of prediction and ground truth vectors.
 
     Args:
