f1fbe46db9ef74bc448d1ff5cced4b9bc20a5f59
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2023-03-23 18:23:59 -0700

    Remove unused expr.

--- rust/benches/benchmark.rs
+++ rust/benches/benchmark.rs
@@ -5,7 +5,6 @@ use std::env::current_dir;
 
 fn criterion_benchmark(c: &mut Criterion) {
     let path = current_dir().unwrap().join("../tests/unit/test_data/sensor_dataset_logs/adcf7d18-0510-35b0-a2fa-b4cea13a6d76/sensors/lidar/315973157959879000.feather");
-    // let path = Path::new("../../tests/unit/test_data/sensor_dataset_logs/adcf7d18-0510-35b0-a2fa-b4cea13a6d76/sensors/lidar/315973157959879000.feather");
     c.bench_function("read_feather", |b| b.iter(|| read_feather(&path, false)));
 }
 
