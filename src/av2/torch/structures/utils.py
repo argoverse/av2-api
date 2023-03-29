8eba1cdd7b3927710ebd8fd3edca0c0c88d372a0
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2023-03-28 18:36:32 -0700

    Update utils.py

--- src/av2/torch/structures/utils.py
+++ src/av2/torch/structures/utils.py
@@ -44,7 +44,7 @@ def SE3_from_frame(frame: pd.DataFrame) -> Se3:
         SE(3) object representing a (N,4,4) tensor of homogeneous transformations.
     """
     quaternion_npy = frame.loc[0, list(QWXYZ_COLUMNS)].to_numpy().astype(float)
-    quat_wxyz = Quaternion(torch.as_tensor(quaternion_npy, dtype=torch.float32))[None]
+    quat_wxyz = Quaternion(torch.as_tensor(quaternion_npy, dtype=torch.float32)[None])
     rotation = So3(quat_wxyz)
 
     translation_npy = frame.loc[0, list(TRANSLATION_COLUMNS)].to_numpy().astype(np.float32)
