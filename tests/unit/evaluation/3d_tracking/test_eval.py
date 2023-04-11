99ae976db232136703f23f4d8a4818fba96852c9
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2023-04-11 12:20:13 -0700

    Add unit test stubs.

---
+++ tests/unit/evaluation/3d_tracking/test_eval.py
@@ -0,0 +1,5 @@
+"""3D Tracking evaluation unit tests."""
+
+def test_evaluate() -> None:
+    """Test 3D tracking evaluation."""
+    pass
---
+++ tests/unit/evaluation/e2e_forecasting/test_eval.py
@@ -0,0 +1,5 @@
+"""End-to-End Forecasting evaluation unit tests."""
+
+def test_evaluate() -> None:
+    """Test End-to-End Forecasting evaluation."""
+    pass
