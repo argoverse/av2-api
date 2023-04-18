f49172aca9acdf694614878cd976bbcec6c40b9a
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2023-04-18 15:13:39 -0700

    Add additional units.

--- src/av2/evaluation/forecasting/utils.py
+++ src/av2/evaluation/forecasting/utils.py
@@ -8,43 +8,43 @@ from av2.utils.typing import NDArrayFloat, NDArrayInt
 from ..typing import ForecastSequences, Frame
 
 
-def agent_velocity(agent: Dict[str, Any]) -> NDArrayFloat:
-    """Get the agent velocity.
+def agent_velocity_m_per_s(agent: Dict[str, Any]) -> NDArrayFloat:
+    """Get the agent velocity_m_per_s.
 
-    Velocity calculated as the delta between the current translation_m and the translation_m in the next timestep.
+    velocity_m_per_s calculated as the delta between the current translation_m and the translation_m in the next timestep.
 
     Args:
         agent: Dictionary containing the agent's translations
 
     Returns:
-        Ground truth velocity for label agents or
+        Ground truth velocity_m_per_s for label agents or
         List of velocities corresponding to each movement prediction for predicted agents
     """
     if "future_translation_m" in agent:  # ground_truth
         return cast(
             NDArrayFloat,
-            (agent["future_translation_m"][0][:2] - agent["current_translation"][:2]) / constants.TIME_DELTA_S,
+            (agent["future_translation_m"][0][:2] - agent["current_translation_m"][:2]) / constants.TIME_DELTA_S,
         )
 
     else:  # predictions
         res = []
         for i in range(agent["prediction"].shape[0]):
-            res.append((agent["prediction"][i][0][:2] - agent["current_translation"][:2]) / constants.TIME_DELTA_S)
+            res.append((agent["prediction"][i][0][:2] - agent["current_translation_m"][:2]) / constants.TIME_DELTA_S)
 
         return np.stack(res)
 
 
-def trajectory_type(agent: Dict[str, Any], class_velocity: Dict[str, float]) -> Union[str, List[str]]:
+def trajectory_type(agent: Dict[str, Any], category_velocity_m_per_s: Dict[str, float]) -> Union[str, List[str]]:
     """Get the trajectory type, which is either static, linear or non-linear.
 
     Trajectory is static if the prediction error of a static forecast is below threshold.
     Trajectory is linear if the prediction error of the linear forecast is below threshold.
-    Linear forecast is created by linearly extrapolating the current velocity.
+    Linear forecast is created by linearly extrapolating the current velocity_m_per_s.
     Trajectory is non-linear if it is neither static or linear.
 
     Args:
-        agent: Dictionary containing the agent's translations and velocity
-        class_velocity: Average velocity of each class, used to determine the prediction error threshold for each class
+        agent: Dictionary containing the agent's translations and velocity_m_per_s.
+        category_velocity_m_per_s: Average velocity_m_per_s of each class, used to determine the prediction error threshold for each category.
 
     Returns:
         String corresponding to the trajectory type for agents in the ground-truth annotations or
@@ -52,12 +52,12 @@ def trajectory_type(agent: Dict[str, Any], class_velocity: Dict[str, float]) -> 
     """
     if "future_translation_m" in agent:  # ground_truth
         time = agent["future_translation_m"].shape[0] * constants.TIME_DELTA_S
-        static_target = agent["current_translation"][:2]
-        linear_target = agent["current_translation"][:2] + time * agent["velocity"][:2]
+        static_target = agent["current_translation_m"][:2]
+        linear_target = agent["current_translation_m"][:2] + time * agent["velocity_m_per_s"][:2]
 
         final_position = agent["future_translation_m"][-1][:2]
 
-        threshold = 1 + constants.FORECAST_SCALAR[len(agent["future_translation_m"])] * class_velocity.get(
+        threshold = 1 + constants.FORECAST_SCALAR[len(agent["future_translation_m"])] * category_velocity_m_per_s.get(
             agent["name"], 0
         )
         if np.linalg.norm(final_position - static_target) < threshold:
@@ -71,10 +71,10 @@ def trajectory_type(agent: Dict[str, Any], class_velocity: Dict[str, float]) -> 
         res: List[str] = []
         time = agent["prediction"].shape[1] * constants.TIME_DELTA_S
 
-        threshold = 1 + constants.FORECAST_SCALAR[len(agent["prediction"])] * class_velocity.get(agent["name"], 0)
+        threshold = 1 + constants.FORECAST_SCALAR[len(agent["prediction"])] * category_velocity_m_per_s.get(agent["name"], 0)
         for i in range(agent["prediction"].shape[0]):
-            static_target = agent["current_translation"][:2]
-            linear_target = agent["current_translation"][:2] + time * agent["velocity"][i][:2]
+            static_target = agent["current_translation_m"][:2]
+            linear_target = agent["current_translation_m"][:2] + time * agent["velocity_m_per_s"][i][:2]
 
             final_position = agent["prediction"][i][-1][:2]
 
