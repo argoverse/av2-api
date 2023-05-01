2b52507e9cfc80eb651846d2d5ffec82f2224bf4
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2023-05-01 16:06:34 -0700

    Use generic floats.

--- rust/src/geometry/utils.rs
+++ rust/src/geometry/utils.rs
@@ -2,14 +2,17 @@
 //!
 //! Geometric utilities.
 
-use ndarray::{s, Array, Ix2};
+use ndarray::{s, Array, Ix2, NdFloat};
 
 /// Convert Cartesian coordinates into Homogeneous coordinates.
 /// This function converts a set of points in R^N to its homogeneous representation in R^(N+1).
-pub fn cart_to_hom(cart: Array<f32, Ix2>) -> Array<f32, Ix2> {
+pub fn cart_to_hom<T>(cart: Array<T, Ix2>) -> Array<T, Ix2>
+where
+    T: NdFloat,
+{
     let num_points = cart.shape()[0];
     let num_dims = cart.shape()[1];
-    let mut hom = Array::<f32, Ix2>::ones([num_points, num_dims + 1]);
+    let mut hom = Array::<T, Ix2>::ones([num_points, num_dims + 1]);
     hom.slice_mut(s![.., ..num_dims]).assign(&cart);
     hom
 }
--- rust/src/so3.rs
+++ rust/src/so3.rs
@@ -2,26 +2,33 @@
 //!
 //! Special Orthogonal Group 3 (SO(3)).
 
-use ndarray::{Array2, ArrayView1};
+use ndarray::{Array, Array2, ArrayView, Ix1, Ix2, NdFloat};
+use num_traits::NumCast;
 
 /// Convert a quaternion in scalar-first format to a 3x3 rotation matrix.
-pub fn quat_to_mat3(quat_wxyz: &ArrayView1<f32>) -> Array2<f32> {
+pub fn quat_to_mat3<T>(quat_wxyz: &ArrayView<T, Ix1>) -> Array<T, Ix2>
+where
+    T: NdFloat,
+{
     let w = quat_wxyz[0];
     let x = quat_wxyz[1];
     let y = quat_wxyz[2];
     let z = quat_wxyz[3];
 
-    let e_00 = 1. - 2. * y.powi(2) - 2. * z.powi(2);
-    let e_01: f32 = 2. * x * y - 2. * z * w;
-    let e_02: f32 = 2. * x * z + 2. * y * w;
+    let one = T::one();
+    let two: T = NumCast::from(2.0).unwrap();
 
-    let e_10 = 2. * x * y + 2. * z * w;
-    let e_11 = 1. - 2. * x.powi(2) - 2. * z.powi(2);
-    let e_12 = 2. * y * z - 2. * x * w;
+    let e_00 = one - two * y.powi(2) - two * z.powi(2);
+    let e_01 = two * x * y - two * z * w;
+    let e_02 = two * x * z + two * y * w;
 
-    let e_20 = 2. * x * z - 2. * y * w;
-    let e_21 = 2. * y * z + 2. * x * w;
-    let e_22 = 1. - 2. * x.powi(2) - 2. * y.powi(2);
+    let e_10 = two * x * y + two * z * w;
+    let e_11 = one - two * x.powi(2) - two * z.powi(2);
+    let e_12 = two * y * z - two * x * w;
+
+    let e_20 = two * x * z - two * y * w;
+    let e_21 = two * y * z + two * x * w;
+    let e_22 = one - two * x.powi(2) - two * y.powi(2);
 
     // Safety: We will always have nine elements.
     unsafe {
