e812d8659e7c6d4944b58e6c0dff89040ebdfdcf
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2023-05-05 12:32:10 -0700

    Update augmentations.

--- rust/src/geometry/augmentations.rs
+++ rust/src/geometry/augmentations.rs
@@ -11,6 +11,8 @@ use crate::geometry::so3::{quat_to_yaw, yaw_to_quat};
 /// Reflect pose across the x-axis.
 pub fn reflect_pose_x(quat_wxyz: ArrayView<f32, Ix2>) -> Array<f32, Ix2> {
     let yaw_rad = quat_to_yaw(quat_wxyz);
+
+    println!("{}", yaw_rad);
     let reflected_yaw_rad = -yaw_rad;
     yaw_to_quat(reflected_yaw_rad.view())
 }
@@ -18,6 +20,6 @@ pub fn reflect_pose_x(quat_wxyz: ArrayView<f32, Ix2>) -> Array<f32, Ix2> {
 /// Reflect pose across the y-axis.
 pub fn reflect_pose_y(quat_wxyz: ArrayView<f32, Ix2>) -> Array<f32, Ix2> {
     let yaw_rad = quat_to_yaw(quat_wxyz);
-    let reflected_yaw_rad = yaw_rad - PI;
+    let reflected_yaw_rad = PI - yaw_rad;
     yaw_to_quat(reflected_yaw_rad.view())
 }
