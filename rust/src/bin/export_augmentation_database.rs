230f5b8d7b18a987d14237abc7e587b9729ff501
Benjamin Wilson <benjaminrwilson@users.noreply.github.com>
2025-01-21 14:16:16 -0800

    Upgrade deps.

--- rust/src/bin/export_augmentation_database.rs
+++ rust/src/bin/export_augmentation_database.rs
@@ -91,7 +91,7 @@ pub fn main() {
 
             let cuboids = sweep.cuboids.unwrap().0;
             let category = cuboids["category"]
-                .utf8()
+                .str()
                 .unwrap()
                 .into_iter()
                 .map(|x| x.unwrap())
@@ -149,22 +149,37 @@ fn _build_data_frame(arr: Array<f32, Ix2>, column_names: Vec<&str>) -> DataFrame
         .into_iter()
         .zip(column_names)
         .map(|(column, column_name)| match column_name {
-            "x" => Series::new("x", column.to_owned().into_raw_vec()),
-            "y" => Series::new("y", column.to_owned().into_raw_vec()),
-            "z" => Series::new("z", column.to_owned().into_raw_vec()),
+            "x" => Series::new("x".into(), column.to_owned().into_raw_vec_and_offset().0),
+            "y" => Series::new("y".into(), column.to_owned().into_raw_vec_and_offset().0),
+            "z" => Series::new("z".into(), column.to_owned().into_raw_vec_and_offset().0),
             "intensity" => Series::new(
-                "intensity",
-                column.to_owned().mapv(|x| x as u8).into_raw_vec(),
+                "intensity".into(),
+                column
+                    .to_owned()
+                    .mapv(|x| x as u8)
+                    .into_raw_vec_and_offset()
+                    .0,
             ),
             "laser_number" => Series::new(
-                "laser_number",
-                column.to_owned().mapv(|x| x as u8).into_raw_vec(),
+                "laser_number".into(),
+                column
+                    .to_owned()
+                    .mapv(|x| x as u8)
+                    .into_raw_vec_and_offset()
+                    .0,
             ),
             "offset_ns" => Series::new(
-                "offset_ns",
-                column.to_owned().mapv(|x| x as u32).into_raw_vec(),
+                "offset_ns".into(),
+                column
+                    .to_owned()
+                    .mapv(|x| x as u32)
+                    .into_raw_vec_and_offset()
+                    .0,
             ),
-            "timedelta_ns" => Series::new("timedelta_ns", column.to_owned().into_raw_vec()),
+            "timedelta_ns" => Series::new(
+                "timedelta_ns".into(),
+                column.to_owned().into_raw_vec_and_offset().0,
+            ),
             _ => panic!(),
         })
         .collect_vec();
